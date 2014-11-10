da5ee3a4c18c0ad9e8a57384f892c289a041fef1
Karl Seguin <karl@openmymind.io>
2014-11-10 10:22:03 +0700

    initial commit

---
+++ jsonwriter.go
@@ -0,0 +1,119 @@
+package jsonwriter
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+	"unicode/utf8"
+)
+
+var (
+	quote        = []byte(`"`)
+	keyStart     = quote
+	keyEnd       = []byte(`":`)
+	escapedQuote = []byte(`\"`)
+	escapedSlash = []byte(`\\`)
+	escapedBS    = []byte(`\b`)
+	escapedFF    = []byte(`\f`)
+	escapedNL    = []byte(`\n`)
+	escapedLF    = []byte(`\r`)
+	escapedTab   = []byte(`\t`)
+)
+
+type Writer struct {
+	W io.Writer
+}
+
+// Creates a JsonWriter that writes to the provided io.Writer
+func New(w io.Writer) *Writer {
+	return &Writer{w}
+}
+
+// Writes a key. The key is placed within quotes and ends
+// with a colon
+func (w *Writer) WriteKey(key string) {
+	w.W.Write(keyStart)
+	w.writeString(key)
+	w.W.Write(keyEnd)
+}
+
+// value can be a string, byte, u?int(8|16|32|64)?, float(32|64)?,
+// time.Time, bool or nil
+func (w *Writer) WriteValue(value interface{}) {
+	switch t := value.(type) {
+	case uint8:
+		w.W.Write([]byte(strconv.FormatUint(uint64(t), 10)))
+	case uint16:
+		w.W.Write([]byte(strconv.FormatUint(uint64(t), 10)))
+	case uint32:
+		w.W.Write([]byte(strconv.FormatUint(uint64(t), 10)))
+	case uint:
+		w.W.Write([]byte(strconv.FormatUint(uint64(t), 10)))
+	case uint64:
+		w.W.Write([]byte(strconv.FormatUint(t, 10)))
+	case int8:
+		w.W.Write([]byte(strconv.FormatInt(int64(t), 10)))
+	case int16:
+		w.W.Write([]byte(strconv.FormatInt(int64(t), 10)))
+	case int32:
+		w.W.Write([]byte(strconv.FormatInt(int64(t), 10)))
+	case int:
+		w.W.Write([]byte(strconv.FormatInt(int64(t), 10)))
+	case int64:
+		w.W.Write([]byte(strconv.FormatInt(t, 10)))
+	case float32:
+		w.W.Write([]byte(strconv.FormatFloat(float64(t), 'g', -1, 32)))
+	case float64:
+		w.W.Write([]byte(strconv.FormatFloat(t, 'g', -1, 64)))
+	case string:
+		w.W.Write(quote)
+		w.writeString(t)
+		w.W.Write(quote)
+	default:
+		panic(fmt.Sprintf("unsuported valued type %v", value))
+	}
+}
+
+// writes a key: value
+// This is the same as calling WriteKey(key) followe by WriteValue(value)
+func (w *Writer) WriteKeyValue(key string, value interface{}) {
+	w.WriteKey(key)
+	w.WriteValue(value)
+}
+
+func (w *Writer) writeString(s string) {
+	start, end := 0, 0
+	var special []byte
+L:
+	for i, r := range s {
+		switch r {
+		case '"':
+			special = escapedQuote
+		case '\\':
+			special = escapedSlash
+		case '\b':
+			special = escapedBS
+		case '\f':
+			special = escapedFF
+		case '\n':
+			special = escapedNL
+		case '\r':
+			special = escapedLF
+		case '\t':
+			special = escapedTab
+		default:
+			end += utf8.RuneLen(r)
+			continue L
+		}
+
+		if end > start {
+			w.W.Write([]byte(s[start:end]))
+		}
+		w.W.Write(special)
+		start = i + 1
+		end = start
+	}
+	if end > start {
+		w.W.Write([]byte(s[start:end]))
+	}
+}
---
+++ jsonwriter_test.go
@@ -0,0 +1,52 @@
+package jsonwriter
+
+import (
+	"bytes"
+	. "github.com/karlseguin/expect"
+	"testing"
+)
+
+type WriterTests struct{}
+
+func Test_Writer(t *testing.T) {
+	Expectify(new(WriterTests), t)
+}
+
+func (_ *WriterTests) WritesAnInt() {
+	assertValue(uint8(1), "1")
+	assertValue(uint16(2), "2")
+	assertValue(uint32(232134), "232134")
+	assertValue(uint64(432434), "432434")
+	assertValue(uint(5), "5")
+	assertValue(int8(-3), "-3")
+	assertValue(int16(-16), "-16")
+	assertValue(int32(-31), "-31")
+	assertValue(int64(-4343), "-4343")
+	assertValue(int(-59922), "-59922")
+}
+
+
+func (_ *WriterTests) WritesAFloat() {
+	assertValue(float32(1.2393), "1.2393")
+	assertValue(float32(-49493.443), "-49493.44")
+	assertValue(float64(99499449.23949), "9.949944923949e+07")
+	assertValue(float64(-3290123.94994), "-3.29012394994e+06")
+}
+
+func (_ *WriterTests) WritesAString() {
+	assertValue(`abc`, `"abc"`)
+	assertValue(`ab"cd`, `"ab\"cd"`)
+	assertValue(`💣`, `"💣"`)
+	assertValue("\\it's\n\tOver\r9000!\\ 💣 💣 💣", `"\\it's\n\tOver\r9000!\\ 💣 💣 💣"`)
+}
+
+func assertValue(value interface{}, expected string) {
+	w, b := n()
+	w.WriteValue(value)
+	Expect(b.String()).To.Equal(expected)
+}
+
+func n() (*Writer, *bytes.Buffer) {
+	b := new(bytes.Buffer)
+	return New(b), b
+}
